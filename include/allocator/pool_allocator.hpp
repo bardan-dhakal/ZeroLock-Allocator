85bb4919db99d621fbc7d51c2151b2853fd7d4ae
Bardan Dhakal <bardandhakal2@gmail.com>
2025-07-29 00:40:31 -0400

    Fixed header file for pool allocator

--- include/allocator/pool_allocator.hpp
+++ include/allocator/pool_allocator.hpp
@@ -1,17 +1,18 @@
+
 #pragma once
 #include <cstddef>
-#include <vector>
 
 class PoolAllocator {
-public:
-    PoolAllocator(size_t block_size, size_t block_count);
-    ~PoolAllocator();
-    void* allocate();
-    void deallocate(void* ptr);
+    public:
+        PoolAllocator(size_t block_size, size_t block_count);
+        ~PoolAllocator();
+        void* allocate();
+        void deallocate(void* ptr);
 
-private:
-    size_t block_size_;
-    size_t block_count_;
-    void* memory_;
-    void* free_list_;
-};
+    private:
+        size_t block_size_;
+        size_t block_count_;
+        void* memory_;
+        void* free_list_;
+
+}
