0fc17621d4f1392d00c9f4f418ed0d8cc043ccab
Bardan Dhakal <bardandhakal2@gmail.com>
2025-12-26 23:56:25 -0600

    Proper use of memcpy and C++ features

--- experiments /mmap_usage.cpp
+++ experiments /mmap_usage.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <sys/mman.h>
+#include <cstring>
 
-void* make_system_call(void * length, size_t size, int prot, int flag, int fd, off_t offset)
+void* allocate_memory(size_t size)
 {
     void* mem_addr;
+    
+    int permissions = PROT_READ | PROT_WRITE; //memory protection - allows for read and write
 
-    mem_addr = mmap(length, size, prot, flag, fd, offset);
+    int flag = MAP_ANONYMOUS | MAP_PRIVATE; //mapping is not backed by any file, memory is raw
+
+    int file_descriptor = -1; //no file required
+
+    off_t offset = 0; //no required offset
+
+    mem_addr = mmap(NULL, size, permissions, flag, file_descriptor, offset);
 
     if  (mem_addr == MAP_FAILED)
     {
@@ -24,18 +33,9 @@ int main()
 
     size_t required_bytes = 4096; //how many bytes you want
 
-    int permissions = PROT_READ | PROT_WRITE; //memory protection - allows for read and write
-
-    int flag = MAP_ANONYMOUS | MAP_SHARED; //mapping is not backed by any file, memory is raw
-
-    int file_descriptor = -1; //no file required
-
-    off_t offset = 0; //no required offset
-
-
     try
     {
-        ptr = make_system_call(NULL, required_bytes, permissions, flag, file_descriptor, offset);
+        ptr = allocate_memory(required_bytes);
         std::cout << "Memory allocated at " << ptr << std::endl;
     }
 
@@ -46,13 +46,23 @@ int main()
     }
 
     char* write_ptr = static_cast<char*>(ptr);
-    char write_data[] = "I wrote data on to the allocated memory";
+    std:: string write_data = "I wrote data on to the allocated memory. I wrote data on to the allocated memory. I wrote data on to the allocated memory. I wrote data on to the allocated memory.";
+    size_t totalBytes = write_data.length();
 
-    write_ptr = write_data;
+    if (totalBytes > required_bytes)
+    {
+        std::cout << "Write data too large" << std::endl;
+        return 1;
+    }
+
+    
+    memcpy(write_ptr, write_data.c_str(), totalBytes);
 
 
     std::string read_data(write_ptr);
     std::cout << "Read data is - " << read_data << std::endl;
+
+    munmap(ptr, required_bytes);
 
 
 
